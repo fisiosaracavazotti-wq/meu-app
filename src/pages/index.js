20cb0b0ef94d27cf24367280c5167e590cf1595a
fisiosaracavazotti-wq <fisiosaracavazotti-wq>
2025-09-09 19:13:47 -0300

    primeiro commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Olá, Sara!</h1>
+      <p>Este é o meu primeiro site com Next.js 🚀</p>
+    </div>
+  )
+}
